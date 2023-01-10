c04d49efb46231577f770e122c5be67ded58a79d
wooSangWook <ww57403@naver.com>
2023-01-10 17:38:27 +0900

    Colavolab api 추가

---
+++ src/api/colabo.ts
@@ -0,0 +1,21 @@
+import { Item, Discount } from "./../store/cartSlice";
+import axios from "axios";
+
+const API_URL =
+  "https://us-central1-colavolab.cloudfunctions.net/requestAssignmentCalculatorData";
+
+export interface Colavolab {
+  items: { [key: string]: Item };
+  discounts: { [key: string]: Discount };
+  currency_code: string;
+}
+
+export async function fetchColavolab() {
+  try {
+    const { data } = await axios.get(API_URL);
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
